736b5bb8ff2b8b0edc97bf083901d910583c1e10
48CONNECT <167672208+patlapanconnect@users.noreply.github.com>
2024-05-03 17:31:43 +0700

    Update player.js

--- player/player.js
+++ player/player.js
@@ -1,40 +1,26 @@
 var video = document.getElementById('video');
-var qualitySelector = document.getElementById('quality');
-
 
 function playM3u8(url){
-if(Hls.isSupported()) {
-    var hls = new Hls();
-    var m3u8Url = decodeURIComponent(atob(url))  // Menggunakan atob untuk mendekripsi URL
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+  if(Hls.isSupported()) {
+      video.volume = 0.3;
+      var hls = new Hls();
+      var m3u8Url = decodeURIComponent(atob(url))  // Menggunakan atob untuk mendekripsi URL
+      hls.loadSource(m3u8Url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
         video.play();
-        // Populate quality selector
-        hls.levels.forEach((level, index) => {
-            var option = document.createElement('option');
-            option.value = index;
-            option.innerText = level.height + 'p'; // Display resolution as label
-            qualitySelector.appendChild(option);
-        });
-    });
-    // Change quality when selected
-    qualitySelector.addEventListener('change', function() {
-        hls.currentLevel = parseInt(qualitySelector.value);
-    });
-    document.title = originalTitle;
-}
-else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-    video.addEventListener('canplay', function() {
-        video.play();
-    });
-    video.volume = 0.3;
-	document.title = originalTitle;
+      });
+      document.title = originalTitle;
     }
+	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+		video.src = url;
+		video.addEventListener('canplay',function() {
+		  video.play();
+		});
+		video.volume = 0.3;
+		document.title = originalTitle;
+  	}
 }
-
-
 
 function playPause() {
     video.paused?video.play():video.pause();
