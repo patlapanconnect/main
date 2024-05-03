ed5ee66b4e4a50e93948b05f8f775203ae30c928
48CONNECT <167672208+patlapanconnect@users.noreply.github.com>
2024-05-03 17:13:54 +0700

    Update player.js

--- player/player.js
+++ player/player.js
@@ -9,18 +9,7 @@ function playM3u8(url){
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED,function() {
         video.play();
-        // Populate quality selector
-        hls.levels.forEach((level, index) => {
-            var option = document.createElement('option');
-            option.value = index;
-            option.innerText = level.height + 'p'; // Display resolution as label
-            qualitySelector.appendChild(option);
-        });
       });
-      // Change quality when selected
-      qualitySelector.addEventListener('change', function() {
-        hls.currentLevel = parseInt(qualitySelector.value);
-    });
       document.title = originalTitle;
     }
 	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
