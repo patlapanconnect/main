2f8bebfc8fd792bf5e1e3a978bdfd5fa80e3413e
48CONNECT <167672208+patlapanconnect@users.noreply.github.com>
2024-05-03 15:47:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ $(window).on('load', function () {
     $('#play-btn').on('click', function () {
         var token = generateToken();  // Membuat token unik
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '?token=' + token + '#' + $('#m3u8-placeholder')[0].value;  // Menambahkan token ke URL
+        window.location.href = './player' + '' + token + '#' + $('#')[0].value;  // Menambahkan token ke URL
     });
 });
