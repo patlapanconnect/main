2e5ec1323b010bd664a015644120acd30bc83573
48CONNECT <167672208+patlapanconnect@users.noreply.github.com>
2024-05-03 16:47:31 +0700

    ???

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 $(window).on('load', function () {
-    var originalTitle = document.title; // Menyimpan judul asli
-
     $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '#' + btoa($('#m3u8-placeholder')[0].value);
-
-        // Mengatur judul dokumen kembali ke judul asli setelah URL berubah
-        document.title = originalTitle;
+        // Mengenkripsi URL tujuan
+        window.location.href = './player' + '#' + btoa($('#m3u8-placeholder')[0].value);  
     });
 });
