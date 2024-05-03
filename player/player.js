8c35d7a90239e658e74380c9b927a7f54a1a7ea8
48CONNECT <167672208+patlapanconnect@users.noreply.github.com>
2024-05-03 17:47:10 +0700

    ???

--- player/player.js
+++ player/player.js
@@ -21,3 +21,44 @@ function playM3u8(url){
 		document.title = originalTitle;
   	}
 }
+
+function playPause() {
+    video.paused?video.play():video.pause();
+}
+
+function volumeUp() {
+    if(video.volume <= 0.9) video.volume+=0.1;
+}
+
+function volumeDown() {
+    if(video.volume >= 0.1) video.volume-=0.1;
+}
+
+function seekRight() {
+    video.currentTime+=5;
+}
+
+function seekLeft() {
+    video.currentTime-=5;
+}
+
+function vidFullscreen() {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen();
+    }
+}
+
+playM3u8(window.location.href.split("#")[1])
+$(window).on('load', function () {
+    $('#video').on('click', function(){this.paused?this.play():this.pause();});
+    Mousetrap.bind('space', playPause);
+    Mousetrap.bind('up', volumeUp);
+    Mousetrap.bind('down', volumeDown);
+    Mousetrap.bind('right', seekRight);
+    Mousetrap.bind('left', seekLeft);
+    Mousetrap.bind('f', vidFullscreen);
+});
