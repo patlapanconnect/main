e735ad3ee8035029aa5f860e4cc64f6631ae27f2
48CONNECT <167672208+patlapanconnect@users.noreply.github.com>
2024-05-03 16:11:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,33 @@ $(window).on('load', function () {
         window.location.href = './player' + '?token=' + token + '#' + btoa($('#m3u8-placeholder')[0].value);  
     });
 });
+
+var express = require('express');
+var app = express();
+
+var tokens = {};
+
+function validateToken(token) {
+    // Fungsi ini memvalidasi token
+    if (tokens[token]) {
+        delete tokens[token];  // Menghapus token dari objek tokens
+        return true;
+    } else {
+        return false;
+    }
+}
+
+app.get('/player', function (req, res) {
+    var token = req.query.token;
+    if (validateToken(token)) {
+        // Jika token valid, tampilkan pemutar video
+        res.send('Pemutar video di sini');
+    } else {
+        // Jika token tidak valid, tampilkan pesan kesalahan
+        res.send('Token tidak valid');
+    }
+});
+
+app.listen(3000, function () {
+    console.log('Aplikasi berjalan di http://localhost:3000');
+});
